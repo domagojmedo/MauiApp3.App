c96eb394156e672ce154795b909cb8734ef4c847
Domagoj Medo <domagoj.medo@moberg.hr>
2022-02-09 23:11:28 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        CounterLabel.Text = $"Current count: {count}";
+
+        SemanticScreenReader.Announce(CounterLabel.Text);
+
+        MediaPicker.CapturePhotoAsync();
+    }
+}
+
